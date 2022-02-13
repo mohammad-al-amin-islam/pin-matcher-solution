e33cd6c9ec4f41a520518f6c596bda7c92ef56c2
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-13 14:02:20 +0600

    calculate button event  added

--- js/app.js
+++ js/app.js
@@ -1,16 +1,34 @@
-function getPin(){
-    const pin = Math.round(Math.random()*10000);
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
     const pinString = pin + "";
-    if(pinString.length==4){
+    if (pinString.length == 4) {
         return pin;
     }
-    else{
-        console.log('less than 4 pin',pin);
+    else {
+        console.log('less than 4 pin', pin);
         return getPin();
     }
 }
 
-function generatePin(){
+function generatePin() {
     const pin = getPin();
     document.getElementById('output-pin').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    //console.log(event.target.innerText);
+    const givenInput = event.target.innerText;
+    const inputField = document.getElementById('display-input');
+
+    if (isNaN(givenInput)) {
+        //console.log('not a number')
+        if(givenInput == 'C'){
+            inputField.value = "";
+        }
+    }
+    else {
+        const previousInput = inputField.value;
+        const output = previousInput + givenInput;
+        inputField.value = output;
+    }
+});
