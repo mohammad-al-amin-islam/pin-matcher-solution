c6a6b259c3e4d27c9c5f955f80cd5be0dd93146e
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-14 22:43:39 +0600

    single delete btn and submit limit added

--- js/app.js
+++ js/app.js
@@ -22,8 +22,11 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
 
     if (isNaN(givenInput)) {
         //console.log('not a number')
-        if(givenInput == 'C'){
+        if (givenInput == 'C') {
             inputField.value = "";
+        }
+        if (givenInput == '<') {
+            inputField.value = inputField.value.slice(0, -1);
         }
     }
     else {
@@ -33,15 +36,25 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
     }
 });
 
-function verifyPin(){
+let loginAttempts = 3;
+function verifyPin() {
     const generatePin = document.getElementById('output-pin').value;
     const typedPin = document.getElementById('display-input').value;
-    if(generatePin == typedPin){
+    if (generatePin == typedPin) {
         document.getElementById('matched-warning').style.display = 'block';
         document.getElementById('mismatch-warning').style.display = 'none';
     }
-    else{
+    else {
         document.getElementById('matched-warning').style.display = 'none';
         document.getElementById('mismatch-warning').style.display = 'block';
+        let failAttempt = --loginAttempts;
+        document.getElementById("action-left").innerText = failAttempt;
+        if(failAttempt == 0){
+            document.getElementById('submit').setAttribute('disabled',true);
+            document.getElementById("all").innerText = 'All Try Finished';
+        }
+
     }
+
 }
+
